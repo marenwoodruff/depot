0a1193555dfadcb8696cc8a83aa4cc0eac03a7a8
Maren Woodruff <wren403@gmail.com>
2015-06-15 09:03:20 -0400

    added the ability for a line_item to count the quantity of an item that a customer wants.

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,4 +2,15 @@ class Cart < ActiveRecord::Base
 	# each line_item contains a reference to a cart_ID
 	# dependent: :destroy, the existence of line items is dependednt on the existence of the cart
 	has_many :line_items, dependent: :destroy
+
+	def add_product(product_id)
+		# find_by- streamlined version of the where() method. instead of returning an array, it returns an existing LineItem or nil
+		current_item = line__items.find_by(product_id: product_id)
+		if current_item
+			current_item.quantity += 1
+		else
+			current_item = line_items.build(product_id: product_id)
+		end
+		current_item
+	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150615124330) do
+ActiveRecord::Schema.define(version: 20150615125211) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
